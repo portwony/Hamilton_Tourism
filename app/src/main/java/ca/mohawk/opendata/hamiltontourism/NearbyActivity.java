8cef52b82349a4e3617c4fdc4d287db429281083
milanp <milanpatel95@live.com>
2018-01-07 17:31:10 -0500

    Added bottom nav and weather code. Weather code does not seem to work.

---
+++ app/src/main/java/ca/mohawk/opendata/hamiltontourism/NearbyActivity.java
@@ -0,0 +1,39 @@
+package ca.mohawk.opendata.hamiltontourism;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class NearbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.action_home:
+                        Intent intentHome = new Intent (NearbyActivity.this, MainActivity.class);
+                        startActivity(intentHome);
+                        break;
+                    case R.id.action_nearby:
+                        Intent intentNearby = new Intent (NearbyActivity.this, NearbyActivity.class);
+                        startActivity(intentNearby);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+    }
+}
